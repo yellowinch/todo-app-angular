a5134bcbba1c6431a5f0003d9cc80b50e864b8b3
Yingqi Huang <yingqi.huang@thoughtworks.com>
2022-06-24 18:04:40 +0800

    feat: connect to backend(edit add remove) still need to implement checked function

--- src/app/component/to-do/to-do.component.ts
+++ src/app/component/to-do/to-do.component.ts
@@ -1,6 +1,12 @@
 import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { Task } from '../../shared/task.model';
 import { TasksService } from '../../../tasks.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'content-Type': 'application/json' }),
+};
 
 @Component({
   selector: 'app-to-do',
@@ -8,46 +14,87 @@ import { TasksService } from '../../../tasks.service';
   styleUrls: ['./to-do.component.scss'],
 })
 export class ToDoComponent implements OnInit {
-  tasks: Task[] = [];
+  editedTask: Task = new Task();
+  tasks: any[] = [];
   toDoTasks: Task[] = [];
   completedTasks: Task[] = [];
+  private serviceUrl = 'http://127.0.0.1:8080/tasks';
   editIndex: number = 0; // 展示当前的input 隐藏label
   constructor(
     private taskService: TasksService,
-    private ref: ChangeDetectorRef
+    private ref: ChangeDetectorRef,
+    private httpClient: HttpClient
   ) {}
   ngOnInit(): void {
-    this.taskService.init();
-    this.tasks = this.taskService.getTasks();
+    this.findTaskList();
     console.log(this.tasks);
     this.onCheck();
   }
   onCheck() {
     setTimeout(() => {
-      this.toDoTasks = this.tasks.filter((task) => !task.done);
-      this.completedTasks = this.tasks.filter((task) => task.done);
+      this.toDoTasks = this.tasks.filter((task) => !task.completed);
+      this.completedTasks = this.tasks.filter((task) => task.completed);
       console.log('this.completedTasks ', this.completedTasks);
     }, 0);
   }
+  getTaskList(): Observable<Task[]> {
+    return this.httpClient.get<Task[]>(this.serviceUrl);
+  }
+  findTaskList(): void {
+    console.log(this.getTaskList());
+    this.getTaskList().subscribe((data) => (this.tasks = data));
+    console.log(this.tasks);
+  }
 
-  clickLabel(id: number) {
+  clickLabel(id: number, task: Task) {
     this.editIndex = id;
+    this.editedTask = task;
     console.log('click id', id);
   }
+  // @ts-ignore
   enter(e: any, id: number) {
     console.log('enter', e.target.value);
-    let newContent: string;
-    newContent = e.target.value.toString();
-    this.taskService.edit(id, newContent);
+    let newName: string;
+    newName = e.target.value.toString();
+    const url = `${this.serviceUrl}/${id}`;
+    this.taskService.edit(id, newName);
+    this.httpClient
+      .put(
+        url,
+        {
+          name: newName,
+          completed: this.editedTask.completed,
+        },
+        httpOptions
+      )
+      .subscribe(() => {
+        this.findTaskList();
+      });
     this.editIndex = -1;
   }
-
-  deleteTask(id: number) {
-    console.log('id', id);
-    this.taskService.delete(id);
-    this.tasks = this.taskService.getTasks();
+  deleteTask(task: Task): void {
+    const id = task.id;
+    const url = `${this.serviceUrl}/${id}`;
+    this.httpClient.delete(url).subscribe();
+    this.tasks = this.tasks.filter((h) => h !== task);
     this.onCheck();
     this.ref.detectChanges();
-    console.log('this.tasks', this.tasks);
   }
+  //
+  // deleteTask(id: number) {
+  //   console.log('id', id);
+  //   this.delete(id);
+  //   this.onCheck();
+  //   this.ref.detectChanges();
+  //   console.log('this.tasks', this.tasks);
+  // }
+  // delete(task: Task | number): Observable<Task> {
+  //   const id = typeof task === 'number' ? task : task.id;
+  //   const url = `${this.serviceUrl}/${id}`;
+  //   const deleteHttpOptions = {
+  //     headers: new HttpHeaders({ 'content-Type': 'application/json' }),
+  //     body: task,
+  //   };
+  //   return this.httpClient.delete(url);
+  // }
 }
